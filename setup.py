85f580af6313594651b3d3c8a893b8111065c883
Lando Loeper <lando.loeper@deutschebahn.com>
2023-11-23 10:36:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(name='bojax', version='0.0.1', packages=find_packages(exclude=['notebooks', 'examples']))
+setup(
+    name='bojax',
+    version='0.0.1',
+    packages=find_packages(exclude=['notebooks', 'examples'])
+)
